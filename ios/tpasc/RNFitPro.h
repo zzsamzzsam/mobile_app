fcc7554446f62963e885d262cb2398b026a77553
ziyao-zhang <might_be_ziyao_zhang@hotmail.com>
2025-11-24 13:13:00 -0500

    Revert "Some updates in order to upload to App Store"
    This reverts commit 3c0e86db3215977d8ef357ab23a01d24b63c3730.

--- ios/tpasc/RNFitPro.h
+++ ios/tpasc/RNFitPro.h
@@ -6,13 +6,24 @@
 //
 
 #import <Foundation/Foundation.h>
-#import <React/RCTBridgeModule.h>
-#import <React/RCTEventEmitter.h>
+#import <React-RCTBridge/RCTBridgeModule.h>
+#import <React-RCTBridge/RCTEventEmitter.h>
 
 @class CBPeripheral; // Forward declaration of CBPeripheral
 @class PBSmartBandSportItemData; // Forward declaration of CBPeripheral
 
 @protocol PJManagerDelegate; // Forward declaration of PJManagerDelegate
+
+@protocol PJManagerDelegate <NSObject>
+- (void)PJManagerDidFindDevice:(CBPeripheral *)peripheral Rssi:(long)rssi Mac:(NSString *)mac VerifyWorld:(NSString *)verifyWorld Info:(NSDictionary *)info;
+- (void)PJManagerDidConnectSuccess:(CBPeripheral *)peripheral;
+- (void)PJManagerDidConnectFail:(CBPeripheral *)peripheral;
+- (void)PJManagerDidFindWriteCharacteristic;
+- (void)PJManagerHeartRateDataWithArray:(NSArray *)array;
+- (void)PJManageMeasureAllWitdArray:(NSArray *)array;
+//- (void)PJManagerSportKeyRealTimeWithSteps:(NSInteger)steps Distance:(NSInteger)distance Calory:(NSInteger)calory;
+//- (void)PJManagerSportKeyRealTimeWithArray:(NSArray <PBSmartBandSportItemData *>*)array;
+@end
 
 @interface RNFitPro : RCTEventEmitter <RCTBridgeModule, PJManagerDelegate>
 @end
